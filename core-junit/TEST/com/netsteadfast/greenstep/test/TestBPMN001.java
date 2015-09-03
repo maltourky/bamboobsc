d04e7bd9eb7bd31404fb95107c062e49d5cdf2b8
billchen198318 <chen.xin.nien@gmail.com>
2015-09-03 22:11:03 +0800

    Update test file

--- core-junit/TEST/com/netsteadfast/greenstep/test/TestBPMN001.java
+++ core-junit/TEST/com/netsteadfast/greenstep/test/TestBPMN001.java
@@ -1,14 +1,22 @@
 package com.netsteadfast.greenstep.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
 
+import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.impl.pvm.process.ProcessDefinitionImpl;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import com.netsteadfast.greenstep.base.AppContext;
@@ -21,14 +29,13 @@ public class TestBPMN001 {
 		InputStream inputStreamBpmn = new FileInputStream( "/home/git/bamboobsc/core-doc/TestProcess001.bpmn" );
 		InputStream inputStreamPng = new FileInputStream( "/home/git/bamboobsc/core-doc/TestProcess001.png" );		
 		RepositoryService repositoryService = (RepositoryService) AppContext.getBean("repositoryService");
-		String deploymentId = repositoryService
+		Deployment deployment = repositoryService
 				.createDeployment()
 				.name("流程測試001")
 				.addInputStream("TestProcess001.bpmn", inputStreamBpmn)
 				.addInputStream("TestProcess001.png", inputStreamPng)				
-				.deploy()
-				.getId();
-		System.out.println("ID: " + deploymentId);
+				.deploy();
+		System.out.println("ID: " + deployment.getId() + " , Name: " + deployment.getName());
 		
 	}
 	
@@ -36,10 +43,86 @@ public class TestBPMN001 {
 	public void startProcess() throws Exception {
 		
 		RuntimeService runtimeService = (RuntimeService) AppContext.getBean("runtimeService");
-		ProcessInstance process = runtimeService.startProcessInstanceById("myProcess:1:4");		
+		ProcessInstance process = runtimeService.startProcessInstanceById("myProcess:1:4");
 		System.out.println("DeploymentId: " + process.getDeploymentId() );
 		System.out.println("ActivityId: " + process.getActivityId() );
 		System.out.println("Name: " + process.getName() );
+		System.out.println("ProcessDefinitionId: " + process.getProcessDefinitionId());
+		System.out.println("ProcessDefinitionKey: " + process.getProcessDefinitionKey());
+		System.out.println("ProcessDefinitionName: " + process.getProcessDefinitionName());
+		
+	}
+	
+	@Test
+	public void deleteProcess() throws Exception {
+		
+		RepositoryService repositoryService = (RepositoryService) AppContext.getBean("repositoryService");
+		repositoryService.deleteDeployment("1");
+		//repositoryService.deleteDeployment("1", true); no need it
+		
+	}
+	
+	@Test
+	public void queryTask() throws Exception {
+		
+		TaskService taskService = (TaskService) AppContext.getBean("taskService");
+		List<Task> tasks = taskService.createTaskQuery().taskAssignee("張三").list();
+		if (tasks == null || tasks.size()<1 ) {
+			System.out.println("no task.");
+			return;
+		}
+		for (Task task : tasks) {
+			this.printTask(task);
+		}
+		
+	}
+	
+	@Test
+	public void queryProcessDefinition() throws Exception {
+		
+		RepositoryService repositoryService = (RepositoryService) AppContext.getBean("repositoryService");
+		ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery();
+		List<ProcessDefinition> processDefs = processDefinitionQuery
+			.processDefinitionKey("myProcess")
+			.orderByProcessDefinitionVersion()
+			.desc()
+			.list();
+		for (ProcessDefinition pd : processDefs) {
+			System.out.println( pd.getId() + " , " + pd.getName() + " , " 
+					+ pd.getKey() + " , " + pd.getVersion() );
+			ProcessDefinitionImpl pdObj = (ProcessDefinitionImpl)repositoryService.getProcessDefinition(pd.getId());
+			System.out.println(pdObj.getActivities());
+		}
+		
+	}
+	
+	@Test
+	public void queryHistory() throws Exception {
+		
+		HistoryService historyService = (HistoryService) AppContext.getBean("historyService");
+		List<HistoricTaskInstance> taskInstances = historyService
+				.createHistoricTaskInstanceQuery()
+				.finished()
+				.list();
+		for (HistoricTaskInstance taskInst : taskInstances) {
+			System.out.println(taskInst.getId() + " , " + taskInst.getName() + " , " 
+					+ taskInst.getFormKey() + " , " + taskInst.getAssignee() );
+		}
+	}
+	
+	@Test
+	public void queryFlowImage() throws Exception {
+		
+		String deploymentId = "1";
+		RepositoryService repositoryService = (RepositoryService) AppContext.getBean("repositoryService");
+		List<String> names = repositoryService.getDeploymentResourceNames(deploymentId);
+		for (String name : names) {
+			System.out.println( name );
+			if (name.endsWith(".png") || name.endsWith(".bmp")) {
+				InputStream is = repositoryService.getResourceAsStream(deploymentId, name);
+				FileUtils.copyInputStreamToFile(is, new File("/tmp/" + name));
+			}
+		}
 		
 	}
 	
@@ -48,16 +131,22 @@ public class TestBPMN001 {
 				
 		TaskService taskService = (TaskService) AppContext.getBean("taskService");
 		List<Task> tasks = taskService.createTaskQuery().list();
-		if (tasks == null ) {
+		if (tasks == null || tasks.size()<1 ) {
 			System.out.println("no task.");
 			return;
 		}
 		for (Task task : tasks) {
+			taskService.setAssignee(task.getId(), "張三");
 			taskService.complete(task.getId());
-			System.out.println("ID: " + task.getId());
-			System.out.println("Name: " + task.getName());
+			this.printTask(task);
 		}
 		
+	}
+	
+	private void printTask(Task task) throws Exception {
+		System.out.println("ID: " + task.getId() + " , Name: " + task.getName() 
+				+ " , FORM_KEY: " + task.getFormKey() 
+				+ " , Assignee: " + task.getAssignee());
 	}
 	
 
