11270a09d789b9ace4c8b0f00f958d6979d3b307
Chen Xin Nien <chen.xin.nien@gmail.com>
2015-10-08 12:15:19 +0800

    Update

---
+++ gsbsc-standard/src/com/netsteadfast/greenstep/bsc/webservice/impl/KpiWebServiceImpl.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright 2012-2016 bambooCORE, greenstep of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package com.netsteadfast.greenstep.bsc.webservice.impl;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import org.springframework.stereotype.Service;
+
+import com.netsteadfast.greenstep.base.AppContext;
+import com.netsteadfast.greenstep.bsc.service.logic.IKpiLogicService;
+import com.netsteadfast.greenstep.bsc.webservice.KpiWebService;
+
+@Service("bsc.webservice.KpiWebService")
+@WebService
+@SOAPBinding
+public class KpiWebServiceImpl implements KpiWebService {
+
+	/**
+	 * for TEST
+	 * 這是測試 SOAP 用的  metod , 暴露 KPIs 主檔資料
+	 * 
+	 */
+	@WebMethod
+	@Override
+	public String outputKpisResult(@WebParam(name="format") String format) throws Exception {
+		IKpiLogicService kpiLogicService = (IKpiLogicService)AppContext.getBean("bsc.service.logic.KpiLogicService");		
+		return kpiLogicService.findKpis(format);
+	}
+
+}
